bef0fcc53e6452821438179c8e9bbe87eeab7e0e
Kerry Mui <kerry.t.mui@gmail.com>
2015-02-02 19:18:55 -0500

    updated application w/ template views for blog posts

--- js/app.js
+++ js/app.js
@@ -1,11 +1,30 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  // put your routes here
+  this.resource('about');
+  this.resource('posts');
+  this.resource('post', {path: ':post_id'})
 });
 
-App.IndexRoute = Ember.Route.extend({
+App.PostsRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return posts;
   }
 });
+
+var posts = [{
+  id: "1",
+  title: "Rails is Omakase",
+  author: { name: "d2h"},
+  date: new Date('12-27-2012'),
+  excerpt: "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum",
+  body: "Bla Bla Bla, Bla Bla Bla, Bla Bla Bla, Bla Bla Bla, Bla Bla Bla, Bla Bla Bla."
+}, {
+
+  id: '2',
+  title: 'The Parley Letter',
+  author: { name: 'd2h'},
+  date: new Date('12-24-2012'),
+  excerpt: "this is an excerpt",
+  body: "This is a body"
+}];
